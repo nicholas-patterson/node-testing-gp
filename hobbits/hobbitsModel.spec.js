4c4092371bdcbcd8c52a34ce328e23eb03bdfe38
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-25 12:16:06 +0200

    wip

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -6,7 +6,7 @@ beforeEach(async () => {
 });
 
 describe('Hobbits.insert', () => {
-  it('is able to add a hobbit to the db!', async () => {
+  it('is able to add hobbits to the db!', async () => {
     // sanity: checking that trucate works, essentially
     let hobbits = await Hobbits.getAll();
     expect(hobbits).toHaveLength(0);
@@ -18,6 +18,25 @@ describe('Hobbits.insert', () => {
 
     // assertion
     expect(hobbits).toHaveLength(2);
+  });
+
+  it('is able to insert the correct hobbits', async () => {
+    // sanity: checking that trucate works, essentially
+    let hobbits = await Hobbits.getAll();
+    expect(hobbits).toHaveLength(0);
+
+    // set up
+    await Hobbits.insert({ name: 'Aragorn' });
+    await Hobbits.insert({ name: 'Sauron' });
+    hobbits = await Hobbits.getAll();
+
+    expect(hobbits[0].name).toBe('Aragorn');
+    expect(hobbits[1].name).toBe('Sauron');
+  });
+
+  it('returns the newly inserted hobbit', async () => {
+    const hobbit = await Hobbits.insert({ name: 'Aragorn' });
+    expect(hobbit.name).toBe('Aragorn');
   });
 });
 
