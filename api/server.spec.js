62a7d3c921929ff664d3313545c732b62b2a3789
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-25 10:47:10 +0200

    wip

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,4 +1,18 @@
-it('works!!!!', () => {
-  // we write assertions, like
-  // foo(5) returns exactly 10 <--- assertions
+function sum(a, b) {
+  return a + b;
+}
+
+describe('sum function', () => {
+  it('sums two numbers!!', () => {
+    // an assert is about asserting
+    // that actual output is equal to expected output
+    const actualOutput = sum(1, 2);
+    const expectedOutput = 3;
+
+    expect(actualOutput).toBe(expectedOutput);
+  });
+  
+  it('returns null if passed an arg which is not a number!!', () => {
+  
+  });
 });
