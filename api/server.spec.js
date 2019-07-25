7e5ef14fd221ed0e78ec9ec8cc3442ac2bfffd65
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-25 10:54:34 +0200

    wip

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,4 +1,10 @@
 function sum(a, b) {
+  if (
+    typeof a !== 'number' ||
+    typeof b !== 'number'
+  ) {
+    return null;
+  }
   return a + b;
 }
 
@@ -6,13 +12,13 @@ describe('sum function', () => {
   it('sums two numbers!!', () => {
     // an assert is about asserting
     // that actual output is equal to expected output
-    const actualOutput = sum(1, 2);
-    const expectedOutput = 3;
 
-    expect(actualOutput).toBe(expectedOutput);
+    expect(sum(1, 2)).toBe(3);
   });
-  
+
   it('returns null if passed an arg which is not a number!!', () => {
-  
+    expect(sum(null, 3)).toBe(null);
+    expect(sum(7, undefined)).toBe(null);
+    expect(sum(7, NaN)).toBe(null);
   });
 });
